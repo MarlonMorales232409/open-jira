8afb17c1f0304c7fb1ec90fbfd3060d4e6da7cd2
MarlonMorales232409 <marlon.morales2324@icloud.com>
2022-10-11 13:41:19 +0200

    Fixed: middlewere fixed

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,26 @@
-// middleware.ts
-import { NextRequest, NextResponse, URLPattern } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-// const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
-
-// This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
+	const checkMongoIDRegExp = new RegExp("^[0-9a-fA-F]{24}$");
+
 	if (request.nextUrl.pathname.startsWith("/api/entries/")) {
 		const id = request.nextUrl.pathname.replace("/api/entries/", "");
-		console.log(id);
+
+		if (!checkMongoIDRegExp.test(id)) {
+			const url = request.nextUrl.clone();
+			url.pathname = "/api/bad-request";
+			url.search = `?message=${id} is not a valid MongoID`;
+			return NextResponse.rewrite(url);
+		}
 	}
 
-	// return NextResponse.redirect(new URL("/entries", request.url));
-
-	// return new Response(JSON.stringify({ message: `Your id is ${25}` }), {
-	// 	status: 200,
-	// 	headers: {
-	// 		"Content-Type": "application/json",
-	// 	},
-	// });
+	return NextResponse.next();
 }
+
+export const config = {
+	//   matcher: '/about/:path*',
+	matcher: [
+		// '/api/:path',
+		"/api/entries/:path",
+	],
+};
